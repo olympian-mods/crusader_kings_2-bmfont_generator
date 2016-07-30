9517fa5503968bd5575f45ec8e168c66cad7dc88
olympian-mods <olympian-mods@users.noreply.github.com>
2016-07-30 10:15:45 +0200

    updated usage

--- src/sh/process.sh
+++ src/sh/process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.0.0-SNAPSHOT"
+VERSION="1.0.0"
 APP_NAME="BMFont Generator: CK2 Converter, ${VERSION}"
 
 function echoAppName() {
@@ -8,20 +8,20 @@ function echoAppName() {
 }
 
 function echoUsage() {
-  echo "Usage: $(basename $0) [OPTION] FONTBASENAME"
+  echo "Usage: $(basename $0) [OPTION] FONTFILE"
   echo "Convert BMFont files for Crusader Kings 2."
   echo ""
   echo "Options:"
   echo "  -v, --version      output version information and exit"
   echo "  -h, --help         display this help and exit"
   echo ""
-  echo "FONTBASENAME is the basename of the font to be converted."
-  echo "For example if the font file is 'my_font.fnt' the FONTBASENAME is 'my_font' without the suffix."
-  echo "In addition to the .fnt file a .tga file is expected with the '_0.tga' suffix, e.g. 'my_font_0.tga'."
+  echo "FONTFILE is the .fnt font file to be converted."
+  echo "In addition to the .fnt file a .tga file is expected in the"
+  echo "same directory with the '_0.tga' suffix, e.g. 'my_font_0.tga'."
   echo "The .tga file will be renamed during the process to match the name of the .fnt file."
   echo ""
   echo "Examples:"
-  echo "  $(basename $0) century_gothic_bold_18"
+  echo "  $(basename $0) /tmp/fonts/century_gothic_bold_18.fnt"
 }
 
 ARG1=$1
@@ -37,10 +37,7 @@ case "${ARG1}" in
   ;;
 esac
 
-
 WORKINGDIR=$(dirname "$(readlink -f "${ARG1}")")
-
-
 FONT_BASENAME=$(basename "${ARG1}" .fnt)
 FONT_FNT=${FONT_BASENAME}.fnt
 FONT_TGA=${FONT_BASENAME}_0.tga
