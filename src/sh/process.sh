79c1cb10c3d0b4ab15bf6d47fbd0945ff634006c
olympian-mods <olympian-mods@users.noreply.github.com>
2016-07-30 09:18:32 +0200

    updated script

--- src/sh/process.sh
+++ src/sh/process.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+VERSION="1.0.0-SNAPSHOT"
+
 FONT_BASENAME=$1
 FONT_FNT=${FONT_BASENAME}.fnt
 FONT_TGA=${FONT_BASENAME}_0.tga
 
 if [ ! -f "${FONT_FNT}" ]
   then
-    echo "Could not find ${FONT_FNT}!"
+    echo "[ERROR] Could not find ${FONT_FNT}!"
 	exit 1
 fi
 
 if [ ! -f "${FONT_TGA}" ]
   then
-    echo "Could not find ${FONT_TGA}!"
+    echo "[ERROR] Could not find ${FONT_TGA}!"
 	exit 1
 fi
+
+echo "BMFont Generator: CK2 Converter, ${VERSION}"
+echo ""
 
 echo "Converting ${FONT_FNT}..."
 sed -i -r -e 's/unicode=0\s*//g' -e 's/\s*outline=0//g' -e 's/\s*chnl=15//g' -e 's/\s*packed=.*//g' -e '3,4d' "${FONT_FNT}"
