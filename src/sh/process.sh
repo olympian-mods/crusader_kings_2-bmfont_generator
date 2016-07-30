c6cfb423a0d56e1381009c1c6738af39e50bbed2
olympian-mods <olympian-mods@users.noreply.github.com>
2016-07-30 08:38:56 +0200

    shell script

---
+++ src/sh/process.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FONT_BASENAME=$1
+FONT_FNT=$FONT_BASENAME.fnt
+FONT_TGA=${FONT_BASENAME}_0.tga
+
+if [ ! -f "$FONT_FNT" ]
+  then
+    echo "Could not find $FONT_FNT!"
+	exit 1
+fi
+
+if [ ! -f "$FONT_TGA" ]
+  then
+    echo "Could not find $FONT_TGA!"
+	exit 1
+fi
+
+sed -i -r -e 's/unicode=0\s*//g' -e 's/\s*outline=0//g' -e 's/\s*chnl=15//g' -e 's/\s*packed=.*//g' -e '3,4d' "$FONT_FNT"
+
+unix2dos "$FONT_FNT"
